3e76b676abf2b9eabc079693e2acf1c83b19fd42
es-yukun <yukun@team.anylinks.jp>
2023-07-01 11:46:46 +0900

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,42 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:33:05 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/28 22:49:12 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/29 22:19:31 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*gnl_read()
+{
+
+}
+
 char	*get_next_line(int fd)
 {
-	static size_t	
+	char		*line;
+	static char	*prev_buf;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	prev_buf = gnl_read(fd, prev_buf);
+
 }
+
+// #include <stdio.h>
+// 
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+// 
+// 	line = "";
+// 	fd = open("get_next_line.h", O_RDONLY);
+// 	while (line)
+// 	{
+// 		line = get_next_line(fd);
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,27 @@
 /*   By: yushsato <yushsato@doche.io>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:29:49 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/28 21:29:53 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/29 20:04:01 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*ret;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ret = (char *)malloc(len1 + len2 + sizeof(char));
+	if (!ret)
+		return (0);
+	ft_bzero(ret, len1 + len2 + 1);
+	ft_memcpy(ret, s1, len1);
+	ft_memcpy(ret + len1, s2, len2);
+	return (ret);
+}
