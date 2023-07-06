cb7c7f83a7a9b5ae379f426d9cefab4f19ef57de
es-yukun <yukun@team.anylinks.jp>
2023-07-06 22:38:01 +0900

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:33:05 by yushsato          #+#    #+#             */
-/*   Updated: 2023/07/04 23:28:13 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/07/06 22:37:17 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*gnl_read(int fd, char *left)
+char	*gnl_until_read(int fd, char c)
 {
 	char	*buf;
 	char	*cache;
@@ -27,32 +27,34 @@ char	*gnl_read(int fd, char *left)
 		cache = ft_strjoin(ret, buf);
 		free(ret);
 		ret = cache;
-		cache = ft_strchr(buf, '\n');
-		if (cahce == NULL)
-			
+		cache = ft_strchr(buf, c);
+		if (len <= 0 || cahce != NULL)
+			break ;
 	}
+	free(buf);
+	return (ret);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*left;
-
+	gnl_until_read(fd, '\n');
 }
 
-// #include <stdio.h>
-// 
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*line;
-// 
-// 	line = "";
-// 	fd = open("get_next_line.h", O_RDONLY);
-// 	while (line)
-// 	{
-// 		line = get_next_line(fd);
-// 		printf("%s", line);
-// 		free(line);
-// 	}
-// 	return (0);
-// }
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	line = "";
+	fd = open("get_next_line.h", O_RDONLY);
+	while (line)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,16 +6,40 @@
 /*   By: yushsato <yushsato@doche.io>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:29:49 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/29 20:04:01 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/07/05 00:34:47 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(char *str)
+{
+	size_t	len;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*s;
+	char	*d;
+
+	if (!dst && !src)
+		return (NULL);
+	d = dst;
+	s = (char *)src;
+	while (n--)
+		*d++ = *s++;
+	return (dst);
+}
+
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	size_t	len1;
 	size_t	len2;
+	size_t	len;
 	char	*ret;
 
 	if (!s1 || !s2)
@@ -25,8 +49,23 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	ret = (char *)malloc(len1 + len2 + sizeof(char));
 	if (!ret)
 		return (0);
-	ft_bzero(ret, len1 + len2 + 1);
+	len = len1 + len2 + 1;
+	while (len--)
+		ret[len] = '\0';
 	ft_memcpy(ret, s1, len1);
 	ft_memcpy(ret + len1, s2, len2);
+	return (ret);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*ret;
+
+	len = ft_strlen(s1) + 1;
+	ret = (char *)malloc(len);
+	if (!ret)
+		return (NULL);
+	ft_memcpy(ret, s1, len);
 	return (ret);
 }
