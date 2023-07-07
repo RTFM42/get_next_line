4acb50bc18872bd8b9bc015ec85225410b170684
es-yukun <yukun@team.anylinks.jp>
2023-07-08 00:41:48 +0900

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:33:05 by yushsato          #+#    #+#             */
-/*   Updated: 2023/07/08 00:40:58 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/07/08 00:41:38 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,21 +65,21 @@ char	*get_next_line(int fd)
 	return (ret);
 }
 
-#include <stdio.h>
-#include <fcntl.h>
-
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	line = "";
-	fd = open("get_next_line.h", O_RDONLY);
-	while (line)
-	{
-		line = get_next_line(fd);
-		printf("%s", line);
-		free(line);
-	}
-	return (0);
-}
+// #include <stdio.h>
+// #include <fcntl.h>
+// 
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+// 
+// 	line = "";
+// 	fd = open("get_next_line.h", O_RDONLY);
+// 	while (line)
+// 	{
+// 		line = get_next_line(fd);
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
